d34978dadea9cd62cc1cea841798cc5e0dcb5d12
fujidig <fuji.rosen@gmail.com>
2011-09-22 20:48:31 +0900

    cancelを実装

--- deferred.js
+++ deferred.js
@@ -1,8 +1,11 @@
 function Deferred() {
 	this._callbackFunc = null;
 	this._errbackFunc = null;
-	this._state = "initial"; // initial, set, fired
+	this._state = "initial"; // initial, set, fired, canceled, canceled-fired
+	this.canceller = null;
 }
+
+Deferred.noop = function() {};
 
 Deferred.prototype = {
 	callback: function (value) {
@@ -12,27 +15,37 @@ Deferred.prototype = {
 		setTimeout(this._errback.bind(this, value));
 	},
 	_callback: function (value) {
-		this._toFired();
+		this._toFired("callback()");
 		if (this._callbackFunc) this._callbackFunc(value);
 	},
 	_errback: function (value) {
-		this._toFired();
+		this._toFired("errback()");
 		if (this._errbackFunc)
 			this._errbackFunc(value);
 		else
 			throw value;
 	},
-	_toFired: function () {
-		if (this._state === "fired") throw new Error("double fire");
-		this._state = "fired";
+	_toFired: function (action) {
+		this._stateCheck(["initial", "set", "canceled"], action);
+		this._state = this._state === "canceled" ? "canceled-fired" : "fired";
 	},
 	then: function(callbackFunc, errbackFunc) {
-		if (this._state === "set") throw new Error("double then");
-		if (this._state === "fired") throw new Error("then after fire");
+		this._stateCheck(["initial"], "then()");
 		this._state = "set";
 		this._callbackFunc = callbackFunc;
 		this._errbackFunc = errbackFunc;
 	},
+	cancel: function () {
+		this._stateCheck(["initial", "set"], "cancel()");
+		if (this.canceller) this.canceller();
+		this._state = "canceled";
+		this._callbackFunc = this._errbackFunc = Deferred.noop();
+	},
+	_stateCheck: function (expected, action) {
+		if (expected.indexOf(this._state) === -1) {
+			throw new Error("can't "+action+" in "+this._state+" state");
+		}
+	}
 };
 
 function returnValue(value) {
@@ -45,16 +58,24 @@ function generatorToDeferred(func) {
 		var generator = func.apply(this, arguments);
 		var callback = function (val) loop(val, false);
 		var errback = function (val) loop(val, true);
+		var waitTask;
+		deferred.canceller = function () {
+			if (waitTask) waitTask.cancel();
+		};
+
 		function loop(val, isError) {
+			waitTask = null;
 			try {
 				if (isError) {
 					generator.throw(val);
 				} else {
 					var ret = generator.send(val);
-					if (ret instanceof returnValue)
+					if (ret instanceof returnValue) {
 						deferred.callback(ret.value);
-					else
-						ret.then(callback, errback);
+					} else {
+						waitTask = ret;
+						waitTask.then(callback, errback);
+					}
 				}
 			} catch (e if e instanceof StopIteration) {
 				deferred.callback();
