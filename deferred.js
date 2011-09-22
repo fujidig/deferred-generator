5c3ac653806b1b2240946853c1d1247d9c74d082
fujidig <fuji.rosen@gmail.com>
2011-09-22 15:37:44 +0900

    例外処理を実装

--- deferred.js
+++ deferred.js
@@ -1,13 +1,31 @@
 function Deferred() {
-	this.listener = null;
+	this._callback = null;
+	this._errback = null;
+	this.state = "initial"; // initial, set, fired
 }
 
 Deferred.prototype = {
-	callback: function(value) {
-		if (this.listener) this.listener(value);
+	callback: function (value) {
+		this._toFired();
+		if (this._callback) this._callback(value);
 	},
-	then: function(callback) {
-		this.listener = callback;
+	errback: function (value) {
+		this._toFired();
+		if (this._errback)
+			this._errback(value);
+		else
+			throw value;
+	},
+	_toFired: function () {
+		if (this.state === "fired") throw new Error("double fire");
+		this.state = "fired";
+	},
+	then: function(callback, errback) {
+		if (this.state === "set") throw new Error("double then");
+		if (this.state === "fired") throw new Error("then after fire");
+		this.state = "set";
+		this._callback = callback;
+		this._errback = errback;
 	},
 };
 
@@ -15,15 +33,23 @@ function generatorToDeferred(func) {
 	return function () {
 		var deferred = new Deferred();
 		var generator = func.apply(this, arguments);
-		function loop(val) {
+		var callback = function (val) loop(val, false);
+		var errback = function (val) loop(val, true);
+		function loop(val, isError) {
 			try {
-				var waitTask = generator.send(val);
-				waitTask.then(loop);
+				if (isError) {
+					generator.throw(val);
+				} else {
+					var waitTask = generator.send(val);
+					waitTask.then(callback, errback);
+				}
 			} catch (e if e instanceof StopIteration) {
 				deferred.callback();
+			} catch (e) {
+				deferred.errback(e);
 			}
 		}
-		loop(undefined);
+		loop(undefined, false);
 		return deferred;
 	};
 }
