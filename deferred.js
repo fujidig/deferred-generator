5c933c14eb3e0114e59edf629e8a281efe6d2e9d
fujidig <fuji.rosen@gmail.com>
2011-09-22 19:38:58 +0900

    this.stateをthis._stateに

--- deferred.js
+++ deferred.js
@@ -1,7 +1,7 @@
 function Deferred() {
 	this._callbackFunc = null;
 	this._errbackFunc = null;
-	this.state = "initial"; // initial, set, fired
+	this._state = "initial"; // initial, set, fired
 }
 
 Deferred.prototype = {
@@ -23,13 +23,13 @@ Deferred.prototype = {
 			throw value;
 	},
 	_toFired: function () {
-		if (this.state === "fired") throw new Error("double fire");
-		this.state = "fired";
+		if (this._state === "fired") throw new Error("double fire");
+		this._state = "fired";
 	},
 	then: function(callbackFunc, errbackFunc) {
-		if (this.state === "set") throw new Error("double then");
-		if (this.state === "fired") throw new Error("then after fire");
-		this.state = "set";
+		if (this._state === "set") throw new Error("double then");
+		if (this._state === "fired") throw new Error("then after fire");
+		this._state = "set";
 		this._callbackFunc = callbackFunc;
 		this._errbackFunc = errbackFunc;
 	},
